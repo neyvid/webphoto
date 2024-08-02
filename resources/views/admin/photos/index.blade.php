466cceaf650bf68e716310443e6e08071d796cdd
navid <n.sakhitabe@gmail.com>
2024-08-02 16:09:02 +0330

    data table config buttons is ok

--- resources/views/admin/photos/index.blade.php
+++ resources/views/admin/photos/index.blade.php
@@ -7,7 +7,6 @@
 @section('content-header')
     تصاویر کاربران
     <a class="btn btn-success" href={{ route('image.create.form') }}>آپلود تصویر جدید</a>
-
 @endsection
 
 @section('content-breadcrumb')
@@ -121,15 +120,73 @@
         </div>
     </div>
 @endsection
-<link rel="stylesheet" href="https://cdn.datatables.net/2.1.2/css/dataTables.dataTables.css" />
+<link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/2.1.2/css/dataTables.dataTables.css" rel="stylesheet">
+<link href="https://editor.datatables.net/extensions/Editor/css/editor.dataTables.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/select/2.0.3/css/select.dataTables.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/buttons/3.1.0/css/buttons.dataTables.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/datetime/1.5.3/css/dataTables.dateTime.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/colreorder/2.0.3/css/colReorder.dataTables.css" rel="stylesheet">
+
+
+
 
 
 
 @section('Customscript')
-<script src="https://cdn.datatables.net/2.1.2/js/dataTables.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#myTable').DataTable();
-    });
-</script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdn.datatables.net/2.1.2/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.1.2/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/select/2.0.3/js/select.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/select/2.0.3/js/select.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/datetime/1.5.3/js/dataTables.dateTime.min.js"></script>
+    <script src="https://cdn.datatables.net/colreorder/2.0.3/js/dataTables.colReorder.js"></script>
+    <script src="https://editor.datatables.net/extensions/Editor/js/dataTables.editor.js"></script>
+    <script src="https://editor.datatables.net/extensions/Editor/js/editor.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/colreorder/2.0.3/js/colReorder.dataTables.js "></script>
+    <script src="https://cdn.datatables.net/buttons/3.1.0/js/dataTables.buttons.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.1.0/js/buttons.dataTables.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.1.0/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/3.1.0/js/buttons.print.min.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+    <script>
+        $('#myTable').DataTable({
+            dom: 'lBfrtip',
+            buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
+            "language": {
+                "lengthMenu": "نمایش _MENU_ مورد در هر صفحه ",
+                "zeroRecords": "متاسفانه موردی یافت نشد!",
+                "info": "نمایش صفحه _PAGE_ از _PAGES_",
+                "infoEmpty": "موردی در دسترس نیست",
+                "infoFiltered": "(فیلترشده از جمع _MAX_ رکورد)",
+                'search': 'جستجو  : ',
+                "oPaginate": {
+                    "sFirst": "اولین",
+                    "sPrevious": "قبلی",
+                    "sNext": "بعدی",
+                    "sLast": "آخرین"
+                }
+            },
+            'paging': true,
+            'lengthChange': true,
+            'searching': true,
+            'ordering': true,
+            'info': true,
+            'autoWidth': true,
+        });
+    </script>
 @endsection
